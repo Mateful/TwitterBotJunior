cc40e685fa2dccefdc233ffc117633720c4adcdd
Benjamin Hoffmann <bechecker91@yahoo.de>
2011-11-04 20:16:16 +0100

    Commit @20:16 CET

--- src/de/fhb/twitterbot/main/TestDriver.java
+++ src/de/fhb/twitterbot/main/TestDriver.java
@@ -8,13 +8,18 @@ import twitter4j.TwitterFactory;
 
 /*
  * app website: https://dev.twitter.com/apps/1364050/show
+ * 
+ * twitter4j.properties:
+ 
+debug=true
+oauth.consumerKey=H75TKHKJuWsI6vS46jO6w
+oauth.consumerSecret=m79nd4QyLYy85wZJRE4eodY5IVog5fhoFLFF5ioqs
+oauth.accessToken=404878505-CybLMDOinWsSFEkTL9wj0i7Gumv7BMwlYicOkSE
+oauth.accessTokenSecret=VE9ZDMu9hwxvVAvk52hVE0uEvxNRavzK23P7D1JoOo
  */
 
 public class TestDriver {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		Twitter twitter = new TwitterFactory().getInstance();
 		Status status;
@@ -37,5 +42,4 @@ public class TestDriver {
 			e.printStackTrace();
 		}
 	}
-
 }
