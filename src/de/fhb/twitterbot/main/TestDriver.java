727f5d149ad32ab4e4bf936f157e65593153f106
Benjamin Hoffmann <bechecker91@yahoo.de>
2011-11-04 15:37:45 +0100

    First commit. Project due to 8th November, 2011, so let's get the party starting!

---
+++ src/de/fhb/twitterbot/main/TestDriver.java
@@ -0,0 +1,13 @@
+package de.fhb.twitterbot.main;
+
+public class TestDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("TwitterBot - Test");
+	}
+
+}
