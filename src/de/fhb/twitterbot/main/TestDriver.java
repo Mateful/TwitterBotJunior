dd684e8f7b7212476bc6fa68b38b33e9b0c190cf
Benjamin Hoffmann <bechecker91@yahoo.de>
2011-11-04 17:45:20 +0100

    First useable Twitter client

--- src/de/fhb/twitterbot/main/TestDriver.java
+++ src/de/fhb/twitterbot/main/TestDriver.java
@@ -1,13 +1,39 @@
 package de.fhb.twitterbot.main;
 
+import twitter4j.DirectMessage;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+/*
+ * app website: https://dev.twitter.com/apps/1364050/show
+ */
+
 public class TestDriver {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("TwitterBot - Test");
+		Twitter twitter = new TwitterFactory().getInstance();
+		Status status;
+		
+		try {
+			status = twitter.updateStatus("new (automated) status message");
+			System.out.println("Successfully updated the status to ["
+					+ status.getText() + "].");
+
+			twitter.createFriendship("kwoxerde");
+			System.out.println("Successfully followed [kwoxerde].");
+
+			DirectMessage message = twitter.sendDirectMessage("kwoxerde",
+					"test message from mateful to kwoxer");
+			System.out.println("Direct message successfully sent to "
+					+ message.getRecipientScreenName());
+		} catch (TwitterException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
