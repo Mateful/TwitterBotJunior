2cc9445c209afd1ad1f5a325036c646838b28994
Benjamin Hoffmann <bechecker91@yahoo.de>
2011-11-06 20:39:28 +0100

    Almost finished!

--- src/de/fhb/twitterbot/main/TestDriver.java
+++ src/de/fhb/twitterbot/main/TestDriver.java
@@ -1,15 +1,7 @@
 package de.fhb.twitterbot.main;
 
-
 /*
  * app website: https://dev.twitter.com/apps/1364050/show
- * 
- * twitter4j.properties:
- * debug=true
- * oauth.consumerKey=H75TKHKJuWsI6vS46jO6w
- * oauth.consumerSecret=m79nd4QyLYy85wZJRE4eodY5IVog5fhoFLFF5ioqs
- * oauth.accessToken=404878505-CybLMDOinWsSFEkTL9wj0i7Gumv7BMwlYicOkSE
- * oauth.accessTokenSecret=VE9ZDMu9hwxvVAvk52hVE0uEvxNRavzK23P7D1JoOo
  */
 
 public class TestDriver {
