8aa5d803de4faf8cbb05e841355c58401bb2d6df
Marco K <armagedons_blade@hotmail.com>
2011-11-07 12:56:17 +0100

    tokennotfoundexception wollte noch ne uid

--- src/de/fhb/twitterbot/Exceptions/TokenNotFoundException.java
+++ src/de/fhb/twitterbot/Exceptions/TokenNotFoundException.java
@@ -1,6 +1,8 @@
 package de.fhb.twitterbot.Exceptions;
 
 public class TokenNotFoundException extends Exception {
+	private static final long serialVersionUID = -8930303975130588474L;
+
 	public TokenNotFoundException() {
 		super();
 	}
